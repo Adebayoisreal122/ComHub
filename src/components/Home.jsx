0dcad0bdf18049728daa7b2b455731a85dd02c7f
adebayo isreal <adebayoisrealight122@gmail.com>
2024-04-11 14:01:32 +0100

    getting throught with the home page and the navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
 <div className='Home '>
 <div className='container w-85 text-center p-5 '>
-<h1 className='text-light welcome'>Welcome to ComHub, your premier destination for hassle-free computer services!</h1>
+<h1 className='text-light welcome'>Welcome to Extreme Computers, your premier destination for hassle-free computer services!</h1>
 <p className='text-light mt-5 expe'>Experience the convenience and peace of mind that comes with choosing [Platform Name] for all your computer service needs. Get started today and let us help you unlock the full potential of your technology!</p>
 </div>
 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,51 +4,31 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-    const handleDropdownToggle = () => {
-      setIsDropdownOpen(!isDropdownOpen);
-    };
-  
-    const handleDropdownClose = () => {
-      setIsDropdownOpen(false);
-    };
-  
-
-
   return (
     <>
-    <nav className="navbar px-4 navbar-expand-lg " style={{boxShadow: '10px 2px 24px rgba(100, 300, 900, 0.9)'}}>
+    <nav className="navbar px-4 navbar-expand-lg " style={{boxShadow: '1px 2px 20px rgba(10, 0, 10, 0.1)'}}>
   <div className="container-fluid">
     <img src="/public/extreme.png" alt="" className="navbar-brand" style={{height:64, width:64}} />
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to={"/"} className="nav-link active" aria-current="page" href="#">Home</Link>
+    <div className="collapse navbar-collapse cola  justify-content-center" id="navbarNavDropdown">
+      <ul className="navbar-nav ">
+        <li className="nav-item ">
+          <Link to={"/"} className="nav-link text-light mx-4 active" aria-current="page" href="#">Home</Link>
         </li>
         <li className="nav-item">
-        <Link to={"/Ourservice"} className="nav-link"   href="#">Our services</Link>
+        <Link to={"/Ourservice"} className="nav-link mx-4 text-light"   href="#">Our services</Link>
         </li>
         <li className="nav-item">
-        <Link to={"/aboutus"} className="nav-link"   href="#">About Us</Link>
+        <Link to={"/aboutus"} className="nav-link mx-4 text-light"   href="#">About Us</Link>
+        </li>
+        <li className="nav-item">
+        <Link to={"/aboutus"} className="nav-link mx-4 text-light"   href="#">Contact</Link>
         </li>
       </ul>
     </div>
-      <ul className="navbar-nav mx-5">
-      <li className="nav-item dropdown" onMouseEnter={handleDropdownToggle} onMouseLeave={handleDropdownClose}>
-          <a className="nav-link dropdown-toggle" href="#" role="button" aria-expanded={isDropdownOpen ? 'true' : 'false'}>
-            Become a seller
-          </a>
-          <ul className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`}>
-            <li><Link to={"/"} className="dropdown-item " href="#">Students</Link></li>
-            <li><Link to={"/"} className="dropdown-item " href="#">Admin</Link></li>
-          </ul>
-        </li>
-      </ul>
-      <button class="btn btn-outline-success" type="submit">Search</button>
+     
   </div>
 </nav>
     </>
